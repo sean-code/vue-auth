792b71ee0c27a4b743153da604263cadbc6e3392
sean-code <john.nganga@student.moringaschool.com>
2023-04-03 10:51:20 +0300

    Create: Navbar as a new component to debulk the app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-light fixed-top">
-      <div class="container">
-        <a href="#" class="navbar-brand">Home</a>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="#" class="nav-link">Login</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Sign up</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+    <NavBar />
     <div class="auth-wrapper">
       <div class="auth-inner">
         Hello
@@ -25,10 +10,13 @@
 </template>
 
 <script>
-
+import NavBar from '../src/components/NavBar.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    NavBar
+  }
 }
 </script>
 
